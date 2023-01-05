9c016d0cb59c7a4300a73db77aae5e5a3e6eb8af
xph9876 <pxu64@atl1-1-02-014-35-2.pace.gatech.edu>
2023-01-05 00:32:36 -0500

    Add preprocessing script for extended human mt genome

---
+++ preprocessing/process_extended_bed.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+def main():
+    parser = argparse.ArgumentParser(description='Process rNMP bed files aligned to extended hmtDNA genome')
+    parser.add_argument('bed', type=argparse.FileType('r'), help='rNMP bed file')
+    parser.add_argument('-o', type=argparse.FileType('w'), default=sys.stdout, help='Output to file')
+    parser.add_argument('-l', type=int, default=16569, help='chrM length (16,569 nt)')
+    args = parser.parse_args()
+
+    args.l += 1
+    # load all rNMPs
+    data = []
+    for l in args.bed:
+        ws = l.rstrip().split('\t')
+        if len(ws) < 6:
+            continue
+        ws[1] = int(ws[1]) % args.l
+        ws[2] = int(ws[2]) % args.l
+        data.append(ws)
+    
+    # output
+    data.sort(key=lambda x:(x[1], x[5]))
+    for d in data:
+        d[1] = str(d[1])
+        d[2] = str(d[2])
+        args.o.write('\t'.join(d) + '\n')
+
+
+    print('Done!')
+
+
+if __name__ == '__main__':
+    main()
+
+        
