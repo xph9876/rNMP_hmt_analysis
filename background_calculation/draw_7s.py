dc91771ff43b4b279afc421f01a8b61c7922ff21
xph9876 <pxu64@atl1-1-02-020-27-1.pace.gatech.edu>
2023-10-13 16:06:25 -0400

    7S DNA

--- background_calculation/draw_7s.py
+++ background_calculation/draw_7s.py
@@ -9,6 +9,26 @@ from collections import defaultdict
 import sys
 
 
+# draw figures
+def draw(df, fea, out):
+    # set color
+    color = defaultdict(lambda:'#AAAAAA')
+    for geno in ['DLTB-8', 'DLTB-P', 'TLTB-8', 'TLTB-P']:
+        color[geno] = '#AAAAFF'
+    sns.set(style='ticks', font_scale=2.5)
+    fig, ax = plt.subplots(figsize=(6, 8), dpi=300)
+    plt.subplots_adjust(bottom=0.45)
+    sns.barplot(x='Sample', y=fea, data=df, palette=color, ax=ax)
+    sns.despine()
+    xmin, xmax = ax.get_xlim()
+    plt.plot([xmin,xmax], [1,1], 'k--')
+    labels = ax.get_xticklabels()
+    ax.set_xticklabels(labels, rotation=90)
+    plt.ylabel('')
+    plt.xlabel('')
+    fig.savefig(out)
+
+
 def main():
     # argparse
     parser = argparse.ArgumentParser(description='Draw sequencing depth bar plot for 7S DNA')
@@ -19,24 +39,10 @@ def main():
     # calculate coverage
     df = pd.read_csv(args.tsv, sep='\t')
 
-    # set color
-    color = defaultdict(lambda:'#AAAAAA')
-    for geno in ['DLTB-8', 'DLTB-P', 'TLTB-8', 'TLTB-P']:
-        color[geno] = '#AAAAFF'
+    # Draw figures
+    draw(df, '7S_EF', args.o + '_ef.png')
+    draw(df, '7S_rev_EF', args.o + '_rev_ef.png')
 
-    # draw barplot
-    sns.set(style='ticks', font_scale=2.5)
-    fig, ax = plt.subplots(figsize=(6, 8), dpi=300)
-    plt.subplots_adjust(bottom=0.45)
-    sns.barplot(x='Sample', y='7S_EF', data=df, palette=color, ax=ax)
-    sns.despine()
-    xmin, xmax = ax.get_xlim()
-    plt.plot([xmin,xmax], [1,1], 'k--')
-    labels = ax.get_xticklabels()
-    ax.set_xticklabels(labels, rotation=90)
-    plt.ylabel('')
-    plt.xlabel('')
-    fig.savefig(f'{args.o}_ef.png')
 
     print('Done!')
 
